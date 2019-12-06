786df6b6b835b63bc0fed9a45394d563de1e0f43
brandonloy <brandon.loy.01@gmail.com>
2019-12-06 14:50:57 -0500

    Turned tracker into an importable function

--- hogFilter.py
+++ hogFilter.py
@@ -48,13 +48,8 @@ def hogFilter(hsv, bbox, cell_size=(8,8)):
     exp = np.zeros(feat.shape)
     learn = np.zeros(9)
     for i in range(0, 9):
-##        h = (filt[:,:,i]-np.mean(filt[:,:,i]))/np.sum(filt[:,:,i])
-##        exp[:,:,i] = cv2.filter2D(feat[:,:,i],-1,h)
         exp[:,:,i], learn[i] = convFilt(filt[:,:,i],feat[:,:,i])
-##        plt.subplot(3,3,1+i)
-##        plt.imshow(filt[:,:,i])
-##    plt.show()
-    #print(learn)
+
     learn = cv2.normalize(learn, learn, 0, 1, cv2.NORM_MINMAX)
     
     return filt, feat, exp, learn
@@ -69,8 +64,6 @@ def convFilt(filt, feat):
     exp - expected filter response
     peak - maximum response value before normalization
     """
-##    h = (filt-np.mean(filt))/np.sum(filt)
-##    exp = cv2.filter2D(feat,-1,h)
 
     filt = filt[::-1,::-1]
     
@@ -83,79 +76,3 @@ def convFilt(filt, feat):
     
     return out, peak
 
-
-#C:\Users\brand\Desktop\VOT\OTB100\Jump\Jump\img
-
-
-##
-##n = 7
-##name = {1:'soccerGuy.jpg',
-##        2:'jump.jpg',
-##        3:'biker.jpg',
-##        4:'surfer.jpg',
-##        5:'girl.jpg',
-##        6:'blurbody.jpg',
-##        7:'skater.jpg'
-##        }
-##box = {1:(211, 144, 248, 261),
-##       2:(136, 35, 52, 182),
-##       3:(262, 94, 16, 26),
-##       4:(275,137,23,26),
-##       5:(57,21,31,45),
-##       6:(400,48,87,319),
-##       7:(57,21,31,45)
-##       }
-##
-##
-##img = cv2.imread(name[n])
-##hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-##
-##
-##
-##
-##bbox = cv2.selectROI(img, False)
-###bbox = box[n]
-##print(bbox)
-###x,y,w,h = bbox
-##h, w, _ = hsv.shape
-##filt, feat, exp, learn = hogFilter(hsv, bbox, cell_size=(4,4))
-##
-##feature = feat[:,:,5]
-##filtr = filt[:,:,5]
-##response = exp[:,:,5]
-##feature = cv2.normalize(feat[:,:,5],feature,0,255,cv2.NORM_MINMAX)
-##filtr = cv2.normalize(filt[:,:,5],filtr,0,255,cv2.NORM_MINMAX)
-##response = cv2.normalize(exp[:,:,5],response,0,255,cv2.NORM_MINMAX)
-####cv2.imwrite('feature.jpg',feature)
-####cv2.imwrite('filter.jpg',filtr)
-####cv2.imwrite('expectedResponse.jpg',response)
-####
-##
-##
-##
-####img2 = cv2.imread('skater2.jpg')
-####hsv2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
-####h, w, _ = hsv2.shape
-#####(0,0,h,w)
-####filt2, feat2, exp2, _ = hogFilter(hsv2, bbox, cell_size=(4,4))
-####
-####
-####resp = np.zeros(feat2.shape)
-####
-####
-####for i in range(0, 9):
-####    resp[:,:,i],_ = (convFilt(filt[:,:,i], feat2[:,:,i]))
-####
-####
-####
-####response = cv2.normalize(exp[:,:,5],resp,0,255,cv2.NORM_MINMAX)
-##print(learn)
-##
-##for i in range(0,9):
-##    plt.subplot(3,9,1 + i)
-##    plt.imshow(filt[:,:,i])
-##    plt.subplot(3,9,10 + i)
-##    plt.imshow(exp[:,:,i])
-##    plt.subplot(3,9,19 + i)
-##    plt.imshow(exp[:,:,i])
-##plt.show()
--- spatialReliability.py
+++ spatialReliability.py
@@ -6,34 +6,6 @@ import time
 import os 
 
 
-vid = {1:'blurbody',
-       2:'jump',
-       3:'liquor',
-       4:'skating2',
-       5:'skating2',
-       6:'surfer',
-       7:'biker',
-       8:'david',
-       9:'MotorRolling',
-       10:'bolt'
-    }
-box = {1:(400,48,87,319),
-       2:(136,35,52,182),
-       3:(256,152,73,210),
-       4:(289,67,64,236),
-       5:(347,58,103,251),
-       6:(275,137,23,26),
-       7:(262,94,16,26),
-       8:(129,80,64,78),
-       9:(117,68,122,125),
-       10:(336,165,26,61)
-       }
-
-
-# This code allows you to specify an object in the foreground
-# The image will be segmented based on color histograms
-# https://docs.opencv.org/3.4/dc/df6/tutorial_py_histogram_backprojection.html
-
 
 ## is a modified Epanechnikov kernel,
 ##k(r; sigma) = 1 − (r/sigma)2, with size parameter sigma equal to the
@@ -51,15 +23,22 @@ def epanK(n):
     ones = np.ones(res.shape)
     res = np.where(res < priorLim, ones*priorLim, res)
     res = np.where(res > 0.9, ones*0.9, res)
-##    print(res)
-##    plt.imshow(res, cmap='gray')
-##    plt.show()
+
     return res
     
 def spatialMap1(img, bbox):
     """
-    img: HSV image
-    Outputs a binary mask used in filter creation
+    Creates a mask for filter creation
+    In the original CSR-DCF, this mask is used as a
+    prior distribution to compute the MAP estimate of the
+    classification of background foreground pixels
+    
+    Input:
+    img - HSV image
+    bbox - Region that contains the object
+
+    Output:
+    out - Binary Mask
     """
     # select object region from image
     #bbox = cv2.selectROI(img, False)
@@ -91,7 +70,6 @@ def spatialMap1(img, bbox):
     objHist = cv2.calcHist([patch], [0,1], None, [180, 256], [0, 180, 0, 256])
     imgHist = cv2.calcHist([img], [0,1], None, [180, 256], [0, 180, 0, 256])
 
-    #https://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/back_projection/back_projection.html
     cv2.normalize(objHist,objHist,0,255,cv2.NORM_MINMAX)
     dst = cv2.calcBackProject([img],[0,1],objHist,[0,180,0,256],1)
     #dst is the appearance likelihood
@@ -109,52 +87,25 @@ def spatialMap1(img, bbox):
     res = np.multiply(res, priorImg)
     out = res
     out = cv2.normalize(res, out, 0, 1, cv2.NORM_MINMAX)
-
-
     
     out*=255
     out = out.astype(np.uint8)
 
-    dot = priorImg*255
-    dot = np.where(dot > 130, np.ones(dot.shape)*255,np.zeros(dot.shape))
-    dot = dot.astype(np.uint8)
-##    print(priorImg.dtype)
-##    plt.imshow(out)
-##    plt.show()
-
-    #out = np.where(out > priorImg, np.ones(out.shape)*0.5, np.zeros(out.shape))
-    #   res is equation 3 in the paper
-    #cv2.imshow("mask_spatrel",out)
-    #cv2.imshow("bakproj",dst)
     return out
 
-
-
-
-def spatialMap1(img, bbox):
-    """
-    GRABCUT 
-    img: HSV image
-    Outputs a binary mask used in filter creation
-    """
-    k = 4
-    smol = cv2.resize(img,None,fx=1/k,fy=1/k)
-    x,y,w,h = bbox
-    x//=k
-    y//=k
-    w//=k
-    h//=k
-    rect = (x,y,w,h)
-    mask = np.zeros(smol.shape[:2],np.uint8)
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1,65),np.float64)
-    cv2.grabCut(smol,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-    out = cv2.resize(mask2,None,fx=k,fy=k)
-    out*=255
-    return out
 
 def spatialMap(img, bbox):
+    """
+    Binary Mask used in filter creation.
+    Segments background and foreground using a canny based approach
+
+    Input:
+    img - HSV image
+    bbox - Region that contains the object
+
+    Output:
+    out - Binary Mask
+    """
     x, y, w, h = bbox
     #x,y,w,h = (240, 69, 98, 119)
     if (w % 2 != 1):
@@ -184,36 +135,8 @@ def spatialMap(img, bbox):
     out = np.zeros(img[:,:,0].shape)
     out[y:y+h,x:x+w] = mask
     
-##    foo = 1
-##    bar = 3
-##    plt.subplot(foo,bar, 1)
-##    plt.imshow(cv2.cvtColor(patch,cv2.COLOR_HSV2RGB))#(out)
-##    plt.subplot(foo, bar, 2)
-##    plt.imshow(edge)
-##    plt.subplot(foo, bar, 3)
-##    plt.imshow(out)
-##    plt.show()
     cv2.imshow('mask',out)
     return out
 
-##seq = 4
-##path = os.path.join('OTB100',vid[seq],vid[seq],'img','0001.jpg')
-##print(path)
-##img = cv2.imread(path)
-##poop = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-##
-###bbox = cv2.selectROI(img, False)
-##bbox = box[seq]
-###print(bbox)
-##start = time.time()
-##res = spatialMap(poop, bbox)
-##end = time.time()
-##
-##print(end-start)
-##print(res.shape)
-##print(img.shape)
-##
-##plt.imshow(res)
-##plt.show()
-##
+
 
